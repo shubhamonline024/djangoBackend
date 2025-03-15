3c8560427ff9d748faec55dd5937981940bbe335
shubhamonline024 <shubhamonline024@gmail.com>
2025-03-15 16:59:52 +0530

    second commit

--- simpleLogin/settings.py
+++ simpleLogin/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pzppbdb2gj-s-x+8ryztkn%(b)i*8e(%hl-vjtmtc!o@43_c14'
+# SECRET_KEY = 'django-insecure-pzppbdb2gj-s-x+8ryztkn%(b)i*8e(%hl-vjtmtc!o@43_c14'
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
